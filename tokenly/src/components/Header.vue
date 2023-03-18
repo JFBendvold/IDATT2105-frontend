5fcef46c36200945df3b6348270d4a4d1471b06d
JFBendvold <j.f.bendvold@gmail.com>
2023-03-18 15:29:09 +0100

    Created header
    Created header

---
+++ tokenly/src/components/Header.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+import '../assets/css/header.css'
+import '@fortawesome/fontawesome-free/css/all.css'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <RouterLink to="/" class="logo">
+        <h1>tokenly</h1>
+      </RouterLink>
+        <div class="search">
+            <input type="text" placeholder="Search" />
+            <button>
+                <i class="fas fa-search fa-sm"></i>
+            </button>
+        </div>
+      <nav class="links">
+        <ul>
+          <li>
+            <RouterLink to="/discover">Discover</RouterLink>
+          </li>
+          <li>
+            <RouterLink to="/sell">Sell</RouterLink>
+          </li>
+          <li>
+            <RouterLink to="/favorites">Favorites</RouterLink>
+          </li>
+          <li>
+            <RouterLink to="/login">Login</RouterLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+</template>
--- tokenly/src/main.js
+++ tokenly/src/main.js
@@ -4,7 +4,8 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-import './assets/main.css'
+import './assets/css/main.css'
+import '@fortawesome/fontawesome-free/css/all.css'
 
 const app = createApp(App)
 
