83b66ccf2b2c2a70c844009a57157d639ed74b9c
JFBendvold <j.f.bendvold@gmail.com>
2023-03-20 11:00:29 +0100

    Signup view
    Created signup-view

--- tokenly/src/components/Login/Login.vue
+++ tokenly/src/components/Login/Login.vue
@@ -1,22 +1,28 @@
 <script setup>
 import { RouterLink } from 'vue-router';
 import '../../assets/css/login/login.css'
+
+let error = ''
+
 </script>
 
 <template>
     <div class="loginContainer">
-        <h1>
-            tokenly
-        </h1>
+        <RouterLink to="/">
+            <h1>
+                tokenly
+            </h1>
+        </RouterLink>
         <div class="login">
             <form>
                 <input type="text" placeholder="Username" />
                 <input type="password" placeholder="Password" />
                 <button type="submit">Login</button>
+                <p class="error" v-if="error">{{ error }}</p>
             </form>
             <p>
                 Don't have an account? 
-                <RouterLink to="/register">
+                <RouterLink to="/signup">
                     Sign up
                 </RouterLink>
             </p>
